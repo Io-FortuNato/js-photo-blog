9b58163483492a09d8c1d2aba1f461b3e76fcad6
io.FortuNato <nomefortunato60@gmail.com>
2026-02-26 10:07:27 +0100

    fix and add bonus responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,20 @@
-const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
-const row = document.querySelector(".row");
+ const row = document.getElementById("row-cards");
 
 fetch("https://lanciweb.github.io/demo/api/pictures/")
   .then(res => res.json())
   .then(data => {
 
-    // Partiamo da indice 0 ma usiamo i+2
-    data.slice(1, 6).forEach((event, index) => {
+   
+    data.slice(0, 6).forEach((event, index) => {  
 
       const col = document.createElement("div");
-      col.className = "col-4 mb-3";
+      col.className = "col-12 col-md-6 col-lg-4 mb-4"; 
 
       col.innerHTML = `
         <div class="card">
           <img class="top-icon" src="./assets/img/pin.svg" alt="Icona in cima">
-          <img src="./assets/img/${index + 2}.png" alt="Immagine card">
+          <img src="./assets/img/${index + 1}.png" class="img-fluid" alt="Immagine card">
           <div class="card-body">
             <div class="card-text">${event.date}</div>
             <h3>${event.title}</h3>
